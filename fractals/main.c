6f57011d9d5c5190148bc3ff29b2fdc23a99737c
SS56Bo <sohamsarkar345@gmail.com>
2026-05-09 00:39:16 +0530

    Fractal Tree done

--- fractals/main.c
+++ fractals/main.c
@@ -2,17 +2,46 @@
 #include <stdio.h>
 #include <math.h>
 
+#define WIDTH 910
+#define HEIGHT 600
+#define FPS 1
+
+#define TREE_COLOR RAYWHITE
+#define THICKNESS 20
+#define BRANCH_OFF_ANGLE 20*DEG2RAD
+
+void DrawBranch(float x, float y, float length, float angle, float thickness) {
+
+    if (length < 1) {
+        return; 
+    }
+    Vector2 start = {x, y};
+
+    float x_end = x + sinf(angle)*length;
+    float y_end = y - cosf(angle)*length;
+    Vector2 end = {x_end, y_end};
+
+    DrawLineEx(start, end, thickness, TREE_COLOR);
+
+    float new_length = length*0.7;
+    float new_R_angle = angle + BRANCH_OFF_ANGLE;
+    float new_L_angle = angle - BRANCH_OFF_ANGLE;
+    float new_thickness = thickness*0.7;
+    DrawBranch(x_end, y_end, new_length, new_L_angle, new_thickness);
+    DrawBranch(x_end, y_end, new_length, new_R_angle, new_thickness);
+}
+
 int main(){
-    const int screenWidth = 800;
-    const int screenHeight = 500;
+    InitWindow(WIDTH, HEIGHT, "Fractal Tree");
+    SetTargetFPS(FPS);
 
-    InitWindow(screenWidth, screenHeight, 'Fractal Window');
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose)
+    while (!WindowShouldClose())
     {
-        /* code */
         BeginDrawing();
+
+            ClearBackground(BLACK);
+            DrawBranch(WIDTH/2, HEIGHT-20, 150, 0, THICKNESS);
+
         EndDrawing();
     }
     
