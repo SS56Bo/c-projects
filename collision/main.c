00d1ad596729e1841b4f0da29000d7dfa1f777aa
SS56Bo <sohamsarkar345@gmail.com>
2026-05-10 12:24:22 +0530

    Collision Sandbox rules setup - Done

--- collision/main.c
+++ collision/main.c
@@ -4,16 +4,41 @@
 
 #define WIDTH 910
 #define HEIGHT 600
-#define FPS 1
+#define FPS 30
+#define PARTICLE_RADIUS 5
+
+typedef struct {
+    float x, y, vx, vy;
+} Particle;
+
+void AddMotionUpdate(Particle *particle) {
+    particle->x += particle->vx;
+    particle->y += particle->vy;
+    if (particle->x > WIDTH-5 || particle->x < 5) {
+        particle->vx = -(particle->vx);
+    }
+    if (particle->y > HEIGHT-5 || particle->y < 5) {
+        particle->vy = -(particle->vy);
+    }
+    //printf("[%f %f]\n", particle->x, particle->y);  DEBUG
+}
+
+void DrawPhysicsParticle(Particle *particle) {
+    DrawCircle(particle->x, particle->y, PARTICLE_RADIUS, GREEN);
+}
 
 int main(){
-    InitWindow(WIDTH, HEIGHT, 'Collision Window');
+    InitWindow(WIDTH, HEIGHT, "Collision Window");
     SetTargetFPS(FPS);
+
+    Particle p = {200, 200, 5, 5};
 
     while (!WindowShouldClose())
     {
-        /* code */
         BeginDrawing();
+            ClearBackground(BLACK);
+            AddMotionUpdate(&p);
+            DrawPhysicsParticle(&p);
         EndDrawing();
     }
     
