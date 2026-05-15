46b6d73b0aa2a07d5b6b9459e242e72568f42e2a
SS56Bo <sohamsarkar345@gmail.com>
2026-05-15 13:48:07 +0530

    Added proper boundaries to our collision particles

--- collision/main.c
+++ collision/main.c
@@ -4,8 +4,8 @@
 
 #define WIDTH 910
 #define HEIGHT 600
-#define FPS 30
-#define PARTICLE_RADIUS 5
+#define FPS 60
+#define PARTICLE_RADIUS 10
 
 typedef struct {
     float x, y, vx, vy;
@@ -15,11 +15,22 @@ typedef struct {
 void AddMotionUpdate(Particle *particle) {
     particle->x += particle->vx;
     particle->y += particle->vy;
+
     if (particle->x > WIDTH-PARTICLE_RADIUS || particle->x < PARTICLE_RADIUS) {
+        if (particle->x > WIDTH-PARTICLE_RADIUS) {
+            particle->x = WIDTH-PARTICLE_RADIUS;
+        } else if (particle->x < PARTICLE_RADIUS){
+            particle->x = PARTICLE_RADIUS;
+        }
         particle->vx = -(particle->vx);
     }
     if (particle->y > HEIGHT-PARTICLE_RADIUS || particle->y < PARTICLE_RADIUS) {
-        particle->vy = -(particle->vy);
+        if (particle->y > HEIGHT-PARTICLE_RADIUS) {
+            particle->y = HEIGHT-PARTICLE_RADIUS;
+        } else if (particle->y < PARTICLE_RADIUS){
+            particle->y = PARTICLE_RADIUS;
+        }
+        particle->vy = -(particle->vy); 
     }
     //printf("[%f %f]\n", particle->x, particle->y);  DEBUG
 }
@@ -34,30 +45,18 @@ void PhysicsBundle(Particle *particle) {
 }
 
 int main(){
-    InitWindow(WIDTH, HEIGHT, "Collision Window");
+    InitWindow(WIDTH, HEIGHT, "Particle Collision");
     SetTargetFPS(FPS);
 
-    Particle p = {200, 200, 10, 10, GREEN};
-    Particle p1 = {110, 60, 10, 8, PURPLE};
-    Particle p2 = {120, 120, 9, 15, YELLOW};
-    Particle p3 = {400, 400, 5, 8, RED};
-    Particle p4 = {90, 90, 6, 8, MAGENTA};
-    Particle p5 = {100, 80, 5, 4, MAROON};
-    Particle p6 = {50, 200, 10, 10, LIME };
-    Particle p7 = {180, 180, 18, 8, LIGHTGRAY};
-
+    Particle p = {200, 200, 10, 10, RED};
+    //Particle p1 = {220, 220, 14, 12, YELLOW};
+       
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(BLACK);
             PhysicsBundle(&p);
-            PhysicsBundle(&p1);
-            PhysicsBundle(&p2);
-            PhysicsBundle(&p3);
-            PhysicsBundle(&p4);
-            PhysicsBundle(&p5);
-            PhysicsBundle(&p6);
-            PhysicsBundle(&p7);
+            DrawFPS(5, 5);
         EndDrawing();
     }
     
