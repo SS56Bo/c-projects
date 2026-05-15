d61d76d619b2023549e14809fbb6d644b45db964
SS56Bo <sohamsarkar345@gmail.com>
2026-05-15 21:39:53 +0530

    electrostatic project update

--- electrostatic/main.c
+++ electrostatic/main.c
@@ -2,17 +2,41 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-    const int screenWidth = 800;
-    const int screenHeight = 500;
+#define WIDTH 900
+#define HEIGHT 600
+#define RADIUS 15
 
-    InitWindow(screenWidth, screenHeight, 'Fractal Window');
+typedef struct  
+{
+    float x, y;
+    float vx, vy;
+    float charge;
+} Electron;
+
+void DrawElectron(Electron *elec) {
+    float charge = elec->charge;
+    if (charge < 0) {
+        DrawCircle(elec->x, elec->y, RADIUS, BLUE);
+    } else {
+        DrawCircle(elec->x, elec->y, RADIUS, RED);
+    }
+}
+
+int main(){
+  
+    InitWindow(WIDTH, HEIGHT, "Electrostatic Window");
     SetTargetFPS(60);
+
+    Electron epos = {WIDTH/2, 120, 0, 0, -6.3};
+    Electron eneg = {WIDTH/2, 250, 0, 0, 2.5};
 
     while (!WindowShouldClose())
     {
-        /* code */
         BeginDrawing();
+            ClearBackground(BLACK);
+            DrawElectron(&epos);
+            DrawElectron(&eneg);
+            DrawFPS(5, 5);
         EndDrawing();
     }
     
